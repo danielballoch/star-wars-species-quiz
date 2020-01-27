337b2d1d5965e4963acaf19191afc9a0a3fd39aa
Daniel Balloch <43333774+danielpbdesign@users.noreply.github.com>
2020-01-27 15:45:41 +1300

    Updates

---
+++ precache-manifest.340486fe53f405bd5f9eec2763d81496.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "370107dc63287c1cc19f0bac8381127b",
+    "url": "/star-wars-species-quiz/index.html"
+  },
+  {
+    "revision": "04bb5dddfae3719e461f",
+    "url": "/star-wars-species-quiz/static/css/main.5a410815.chunk.css"
+  },
+  {
+    "revision": "2546ba638b627d9ef79c",
+    "url": "/star-wars-species-quiz/static/js/2.59c021fe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/star-wars-species-quiz/static/js/2.59c021fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "04bb5dddfae3719e461f",
+    "url": "/star-wars-species-quiz/static/js/main.a744038f.chunk.js"
+  },
+  {
+    "revision": "ba7bae4824504f3cb5f8",
+    "url": "/star-wars-species-quiz/static/js/runtime-main.8a7cd8aa.js"
+  },
+  {
+    "revision": "4a6b63222f9571feddd5ed5e2cfab302",
+    "url": "/star-wars-species-quiz/static/media/Dug.4a6b6322.PNG"
+  },
+  {
+    "revision": "8e26cbbbb64f03511396e1dbab3973af",
+    "url": "/star-wars-species-quiz/static/media/Ewok.8e26cbbb.PNG"
+  },
+  {
+    "revision": "eb9d5331ed5741dbfffacedc9fa8ef07",
+    "url": "/star-wars-species-quiz/static/media/Gungan.eb9d5331.PNG"
+  },
+  {
+    "revision": "03c9f5310c193f7ce59cc13433817943",
+    "url": "/star-wars-species-quiz/static/media/Hutt.03c9f531.jpg"
+  },
+  {
+    "revision": "1fb3d2a3b5e37ffc0bf273a9796e0957",
+    "url": "/star-wars-species-quiz/static/media/MonCalamari.1fb3d2a3.PNG"
+  },
+  {
+    "revision": "987b962f5b33f8855ea79ce0e2b427d6",
+    "url": "/star-wars-species-quiz/static/media/Neimodian.987b962f.PNG"
+  },
+  {
+    "revision": "7ab51776dba2176df127e1416ef1571f",
+    "url": "/star-wars-species-quiz/static/media/Sullustan.7ab51776.PNG"
+  },
+  {
+    "revision": "c8fcb77f055557b82dcc3ed1b17c0518",
+    "url": "/star-wars-species-quiz/static/media/Toydarian.c8fcb77f.PNG"
+  },
+  {
+    "revision": "96fff2bfaf90bb37b0299f3ff64a6aa8",
+    "url": "/star-wars-species-quiz/static/media/Trandoshan.96fff2bf.PNG"
+  },
+  {
+    "revision": "45a26b67b1804f5a92a48e97cb52aadb",
+    "url": "/star-wars-species-quiz/static/media/background.45a26b67.png"
+  },
+  {
+    "revision": "add64530c1618fb8f22a98a8d3356cd9",
+    "url": "/star-wars-species-quiz/static/media/yoda.add64530.jpg"
+  }
+]);
